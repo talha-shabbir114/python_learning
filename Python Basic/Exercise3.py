18625d1d0542c5f119d7112db7d6455e02e51287
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-07 04:55:24 +0500

    hi string for printing

---
+++ Python Basic/Exercise3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 04:52:25 2022
+
+@author: Talha Khokhar
+"""
+
+# Display a string directly
+print("hello")
+# Display the contents of a string variable
+my_string = "hi"
+print(my_string)
