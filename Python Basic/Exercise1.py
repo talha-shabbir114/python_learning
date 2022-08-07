44d7a969f1de2ec3c3f90b10e79a7f5105ddbc96
talha-shabbir114 <110746927+talha-shabbir114@users.noreply.github.com>
2022-08-07 05:04:23 +0500

    Python basic (#1)
    * done print hi in the file
    
    * write string hello
    
    * hi string for printing

---
+++ Python Basic/Exercise1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 04:12:37 2022
+
+@author: Talha Khokhar
+"""
+
+print("hi")
---
+++ Python Basic/Exercise2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 04:37:40 2022
+
+@author: Talha Khokhar
+"""
+
+my_string = "hello"
+print(my_string)
