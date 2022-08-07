0e65c93514b0f18e110df4035e34652b66481d44
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-08 01:54:30 +0500

    hello 55

---
+++ Python Comments/Exercise3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 01:47:15 2022
+
+@author: Talha Khokhar
+"""
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
