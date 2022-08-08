3463f7481a64cf86a89788ad890ba036afb0b165
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-08 21:42:23 +0500

    print integer float and string variables

---
+++  Python Variables/Exercise1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 20:32:16 2022
+
+@author: Talha Khokhar
+"""
+
+counter = 100 # An integer assignment
+miles = 1000.0 # A floating point
+name = "John" # A string
+print (counter)
+print (miles)
+print (name)
