0c889b97e6a29adc89cf7fc12c31236512366616
talha-shabbir114 <110746927+talha-shabbir114@users.noreply.github.com>
2022-08-09 00:37:47 +0500

    Python variables (#3)
    * print integer float and string variables
    
    * assign multiple variables in a single line
    
    * string assignment and concatination
    
    * try to add stiring with intiger
    
    * take input variables and get product and print them

---
+++  Python Variables/Exercise3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 20:32:16 2022
+
+@author: Talha Khokhar
+"""
+
+x = "awesome"
+print("Python is " + x)
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+
