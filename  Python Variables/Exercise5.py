292252b2850f2f0b8b914a5e287c68e194812a82
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-08 21:47:23 +0500

    take input variables and get product and print them

---
+++  Python Variables/Exercise5.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 20:32:16 2022
+
+@author: Talha Khokhar
+"""
+
+# Get two numbers from the user, multiply them,
+# and display the result
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+product = float(a) * float(b)
+print("The product of " + a + " and " + b + " is " + str(product) + ".")
+
