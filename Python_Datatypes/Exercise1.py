842c3ca1085b663e8703c73b736f0ad154f0a14a
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-10 15:46:36 +0500

    int, float and complex

---
+++ Python_Datatypes/Exercise1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 15:28:23 2022
+
+@author: Talha Khokhar
+"""
+
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ Python_Datatypes/Exercise2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 15:32:01 2022
+
+@author: Talha Khokhar
+"""
+
+x = 1 # int
+y = 2.8 # float
+z = 5+1j # complex
+#convert from int to float:
+a = float(x)
+#convert from float to int:
+b = int(y)
+#convert from int to complex:
+c = complex(x)
+print(a)
+print(b)
+print(c)
+3
+print(type(a))
+print(type(b))
+print(type(c))
