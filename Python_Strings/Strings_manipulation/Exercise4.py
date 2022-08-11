cba63c93cff88775fafc424110c52153044e4aab
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-12 01:22:30 +0500

    string startswith

---
+++ Python_Strings/Strings_manipulation/Exercise4.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 22:47:15 2022
+
+@author: Talha Khokhar
+"""
+
+# Exercise 4
+string1 = "Becomes"
+string2 = "becomes"
+string3 = "BEAR"
+string4 = " bEautiful"
+print(string1.startswith("be"))
+print(string2.startswith("be"))
+print(string3.startswith("be"))
+print(string4.startswith("be"))
