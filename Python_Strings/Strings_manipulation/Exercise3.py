baf529c2babc1625b63b54cc347e445f10ae226f
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-12 01:20:14 +0500

    string strip

---
+++ Python_Strings/Strings_manipulation/Exercise3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 22:45:45 2022
+
+@author: Talha Khokhar
+"""
+
+# Exercise 3
+string1 = " Filet Mignon"
+string2 = "Brisket "
+string3 = " Cheeseburger "
+print(string1.strip()) # Could also use .lstrip()
+print(string1.strip()) # Could also use .rstrip()
+print(string3.strip())
