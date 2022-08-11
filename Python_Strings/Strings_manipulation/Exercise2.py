999b334a495187c5e73f25c3d4de5477862c4f78
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-12 01:18:33 +0500

    string in upper case

---
+++ Python_Strings/Strings_manipulation/Exercise2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 22:43:40 2022
+
+@author: Talha Khokhar
+"""
+
+# Exercise 2
+string1 = "Animals"
+string2 = "Badger"
+string3 = "Honey Bee"
+string4 = "Honeybadger"
+print(string1.upper())
+print(string2.upper())
+print(string3.upper())
+print(string4.upper())
