25c112d818216be844c504d987c1eb91b5604295
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-12 00:43:26 +0500

    find length of the string

---
+++ Python_Strings/Indexing_slicing/Exercise1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 15:56:26 2022
+
+@author: Talha Khokhar
+"""
+
+# Display the number of letters in the string
+my_word = "antidisestablishmentarianism"
+print(len(my_word))
