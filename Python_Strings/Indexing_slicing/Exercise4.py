72bae7fcffe84baa37ec4e363f692792c011d675
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-12 00:57:39 +0500

    string subscription

---
+++ Python_Strings/Indexing_slicing/Exercise4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 16:01:12 2022
+
+@author: Talha Khokhar
+"""
+
+# Exercise 4
+# Use subscripting to display part of a string
+print("bazinga"[2:6])
