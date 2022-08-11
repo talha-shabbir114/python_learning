604c089840bcc90b55865b263a0af808c27d73aa
talha-shabbir114 <110746927+talha-shabbir114@users.noreply.github.com>
2022-08-12 00:23:55 +0500

    int, float and complex (#4)

---
+++ Python_Datatypes/Exercise3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 15:34:34 2022
+
+@author: Talha Khokhar
+"""
+
+# int
+x = int(1) # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+#float
+x = float(1) # x will be 1.0
+y = float(2.8) # y will be 2.8
+z = float("3") # z will be 3.0
+w = float("4.2") # w will be 4.2
+#string
+x = str("s1") # x will be 's1'
+y = str(2) # y will be '2'
+z = str(3.0) # z will be '3.0'
+print(f"{3 ** .125:.3f}")
+print(f"${150000:,.2f}")
+print(f"{2 / 10:.0%}")
