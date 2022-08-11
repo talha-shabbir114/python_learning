1170e71cf1fbe4d579263b696dc791e9e1c38c3e
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-12 00:44:24 +0500

    sting concatination

---
+++ Python_Strings/Indexing_slicing/Exercise2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 15:58:06 2022
+
+@author: Talha Khokhar
+"""
+
+# Concatenate two strings together
+string_left = "bat"
+string_right = "man"
+print(string_left + string_right)
