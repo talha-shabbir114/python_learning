43bcee71d2da1be4a7eb7b9dd3693374ba0cc35e
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-12 01:48:40 +0500

    find upper case X

---
+++ Python_Strings/String_string/Exercise3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 23:07:21 2022
+
+@author: Talha Khokhar
+"""
+
+# Exercise 3
+# Try to find an upper-case "X" in user input:
+my_input = input("Type something: ")
+print(my_input.find("X"))
