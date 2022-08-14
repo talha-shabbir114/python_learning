aa74edf3968c8cca77570447b6a2e45dce279560
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-15 04:43:01 +0500

    list item removed

---
+++ Python_Data_Structure/List/Exercise4.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 03:59:11 2022
+
+@author: Talha Khokhar
+"""
+
+# Remove the second item
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+# Remove the first item
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
