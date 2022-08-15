c16336c2244e4610db8e68028bbed0b382347af4
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-15 05:14:18 +0500

    pop method used in dictionary

---
+++ Python_Data_Structure/Dictionaries/Exercise3.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 04:11:52 2022
+
+@author: Talha Khokhar
+"""
+
+# The pop() method removes the item with the specified key name:
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead)
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+# The del keyword removes the item with the specified key name
+thisdict = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+del thisdict["model"]
+print(thisdict)
