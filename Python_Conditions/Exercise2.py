9535ebce5859590ffc4094918a7b6f601db2721a
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-22 00:04:15 +0500

    Conditions if and elif are applied

---
+++ Python_Conditions/Exercise2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 17 02:01:54 2022
+
+@author: Talha Khokhar
+"""
+
+# One line if else statement:
+a = 2
+b = 330
+print("A") if a > b else print("B")
+# One line if else statement, with 3 conditions
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
