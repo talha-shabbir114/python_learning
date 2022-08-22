928cf9dae49225bce1ac973238ee8bfa94d1cc01
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-22 07:18:33 +0500

    Using For loop to print upper and lower case

---
+++ Python_Loops/For_Loop/Exercise4.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 22 01:52:43 2022
+
+@author: Talha Khokhar
+"""
+
+uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+for upper, lower in zip(uppercase, lowercase):
+    print(upper, lower)
