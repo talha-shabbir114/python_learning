b419fb75203bd75c484aa14e79fae714df6ae121
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-22 07:15:19 +0500

    Using for loop to print list, int & str

---
+++ Python_Loops/For_Loop/Exercise2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 22 01:45:52 2022
+
+@author: Talha Khokhar
+"""
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+    for y in fruits:
+        print(x, y)
+for x in [0, 1, 2]:
+    pass
