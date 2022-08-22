5d409ae42792cc70211d6cb81e567a9494d7543d
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-22 06:59:40 +0500

    Print all items using while loop

---
+++ Python_Loops/While_Loop/Exercise1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 22 01:22:31 2022
+
+@author: Talha Khokhar
+"""
+
+# Print all items, using a while loop to go through all the index numbers
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+    print(thislist[i])
+    i = i + 1
