25823333e5b89cc40c08fe7246d4956571fd65f3
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-25 01:13:51 +0500

    Define my function with args

---
+++ Python_Functions/Exercise2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 22 02:12:02 2022
+
+@author: Talha Khokhar
+"""
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
+    
+my_function_with_args("talha", "are okay")
