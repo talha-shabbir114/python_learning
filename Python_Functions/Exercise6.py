4263224150386756fa1aa7196d81fd6628d289a7
talha-shabbir114 <khokhartalha114@gmail.com>
2022-08-25 01:32:26 +0500

    Using if, else and elif statement

---
+++ Python_Functions/Exercise6.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 22 02:24:10 2022
+
+@author: Talha Khokhar
+"""
+
+python
+if x > 0:
+         return 'positive'
+elif x < 0:
+         return 'negative'
+else:
+         return 'zero'
+for x in [-1, 0, 1]:
+    print(sign(x))
